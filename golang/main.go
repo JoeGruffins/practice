87fffae876cd9806994ca229da05163b0359f4fc
JoeGruff <joegruffins@gmail.com>
2026-04-07 18:41:41 +0900

    fix

--- golang/main.go
+++ golang/main.go
@@ -306,8 +306,10 @@ func productExceptSelf(nums []int) []int {
 	for i := 1; i < len(nums); i++ {
 		answer[i] = answer[i-1] * nums[i-1]
 	}
+	product := 1
 	for i := len(nums) - 2; i >= 0; i-- {
-		answer[i] = answer[i+1] * nums[i+1]
+		product *= nums[i+1]
+		answer[i] *= product
 	}
 	return answer
 }
